158368ed28f9193ab64f958b76d232382e25c863
Avo B <58011124+AvoBolivar@users.noreply.github.com>
2022-02-19 22:33:15 -0500

    Create Images.py

---
+++ Images.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+imp_idle = [pygame.image.load("imp_idle_anim_f0.png"), pygame.image.load("imp_idle_anim_f1.png"),
+            pygame.image.load("imp_idle_anim_f2.png"), pygame.image.load("imp_idle_anim_f3.png")]
+
+imp_run = [pygame.image.load("imp_run_anim_f0.png"), pygame.image.load("imp_run_anim_f1.png"),
+           pygame.image.load("imp_run_anim_f2.png"), pygame.image.load("imp_run_anim_f3.png")]
+
+coin_bag = [pygame.image.load("coin_bag-1.png"), pygame.image.load("coin_bag-2.png"),
+            pygame.image.load("coin_bag-3.png"), pygame.image.load("coin_bag-4.png")]
+
+coin_icon = [pygame.image.load("coin_anim_f0.png"), pygame.image.load("coin_anim_f1.png"),
+             pygame.image.load("coin_anim_f2.png"), pygame.image.load("coin_anim_f3.png")]
